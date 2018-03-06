0e494414852ad75a24fa6ae6c9226d6628f5fd32
Andrey Lushnikov <lushnikov@chromium.org>
2018-03-06 14:45:37 +0300

    add proper title

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const mapElement = document.querySelector('.worldmap');
-const mapLoaded = new Promise(resolve => mapElement.onload = resolve);
-
 window.addEventListener('DOMContentLoaded', async () => {
+  const mapElement = document.querySelector('.worldmap');
+  const mapLoaded = new Promise(resolve => mapElement.onload = resolve);
+
   const response = await fetch('./countries.txt');
   const text = await response.text();
   const visited = new Set();
+  const names = new Map();
   for (const entry of text.split('\n')) {
-    if (!entry.startsWith('+'))  
+    if (!entry.startsWith('+'))
       continue;
     const countryCode = entry.substring(1).trim().split(' ')[0];
     visited.add(countryCode);
@@ -25,7 +26,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   // Mark countries as visited and add "title" to them all.
   const countries = svgDocument.querySelectorAll('svg > g > path');
   for (const country of countries) {
-    const titleElement = svgDocument.createElement('title');
+    const titleElement = svgDocument.createElementNS('http://www.w3.org/2000/svg', 'title');
     titleElement.textContent = country.getAttribute('title');
     country.appendChild(titleElement);
     if (visited.has(country.id))
