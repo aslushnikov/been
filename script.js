7ddb3e8b19a2a111588ad22abb61cb7f859ccbba
Andrey Lushnikov <lushnikov@chromium.org>
2018-07-02 18:20:44 +0100

    refactor: remake everything while flying to STP

--- script.js
+++ script.js
@@ -1,72 +1,135 @@
 class Country {
-  constructor(id, name, element) {
+  constructor(id, name, element, visited) {
     this.id = id;
     this.name = name;
-    this.element = element;
-    this.element.__country = this;
-    this._visited = false;
-  }
+    this.visited = visited;
+    this.mapElement = element;
+    this.mapElement.classList.toggle('visited', visited);
+    this.mapElement[Country.Symbol] = this;
+    // Create title for country.
+    const titleElement = element.ownerDocument.createElementNS('http://www.w3.org/2000/svg', 'title');
+    titleElement.textContent = this.name;
+    this.mapElement.appendChild(titleElement);
 
-  setVisited(visited) {
-    this.element.classList.toggle('visited', visited);
-  }
-
-  visited() {
-    return this.element.classList.contains('visited');
+    // Create sidebar entry.
+    this.sidebarElement = document.createElement('div');
+    this.sidebarElement.classList = 'entry';
+    this.sidebarElement.classList.toggle('visited', visited);
+    this.sidebarElement.textContent = this.name;
+    this.sidebarElement[Country.Symbol] = this;
   }
 }
 
-function createCountries(svgDocument, mapData) {
-  const countries = [];
-  for (let entry of mapData.trim().split('\n')) {
-    const visited = entry.startsWith('+');
-    if (visited)
-      entry = entry.substring(1);
-    entry = entry.trim();
-    const id = entry.substring(0, 2);
-    const name = entry.substring(2).trim();
-    const element = svgDocument.querySelector('#' + id);
-    const country = new Country(id, name, element);
-    country.setVisited(visited);
-    countries.push(country);
+Country.Symbol = Symbol('country');
+
+class Region {
+  constructor(name) {
+    this.name = name;
+    this.countries = [];
   }
-  countries.sort((a, b) => a.name.localeCompare(b.name));
-  return countries;
 }
 
-window.addEventListener('DOMContentLoaded', async () => {
-  // Load all the assets.
-  const mapElement = document.querySelector('.worldmap');
-  const mapLoaded = new Promise(resolve => mapElement.onload = resolve);
-  const mapData = await fetch('./countries.txt').then(response => response.text());
-  await mapLoaded;
-
-  // Parse list into countries array.
-  const svgDocument = mapElement.contentWindow.document;
-  const countries = createCountries(svgDocument, mapData);
-
-  // Add our styles to the nested SVG.
-  const linkElm = svgDocument.createElementNS('http://www.w3.org/1999/xhtml', 'link');
-  linkElm.setAttribute('href', 'mapstyles.css');
-  linkElm.setAttribute('type', 'text/css');
-  linkElm.setAttribute('rel', 'stylesheet');
-  svgDocument.querySelector('defs').appendChild(linkElm);
-
-  // Add tooltips to countries.
-  for (const country of countries) {
-    const titleElement = svgDocument.createElementNS('http://www.w3.org/2000/svg', 'title');
-    titleElement.textContent = country.name;
-    country.element.appendChild(titleElement);
+class Map {
+  static async create() {
+    const [svgText, countriesText] = await Promise.all([
+      fetch('./world.svg').then(response => response.text()),
+      fetch('./countries.txt').then(response => response.text())
+    ]);
+    const domParser = new DOMParser();
+    const parsedDocument = domParser.parseFromString(svgText, 'text/html');
+    const foreignSVG = parsedDocument.querySelector('svg');
+    const svg = document.importNode(foreignSVG, true);
+    return new Map(svg, countriesText);
   }
 
-  const countriesList = document.querySelector('.countries');
-  for (const country of countries) {
-    if (!country.visited())
-      continue;
-    const div = document.createElement('div');
-    div.classList.add('country');
-    div.textContent = country.name;
-    countriesList.appendChild(div);
+  constructor(svg, countriesText) {
+    this.regions = [];
+    let region = null;
+    for (let entry of countriesText.split('\n')) {
+      entry = entry.trim();
+      if (!entry.length)
+        continue;
+      if (entry.startsWith('# ')) {
+        region = new Region(entry.substring(2).trim());
+        this.regions.push(region);
+      } else {
+        const match = entry.match(/^-\s*\[(.*)\]\s+([A-Za-z]{2})\s+(.*)$/);
+        if (!match) {
+          console.error('Failed to parse line!\n  ' + entry);
+          continue;
+        }
+        const visited = !!match[1].trim();
+        const id = match[2];
+        const name = match[3];
+        const element = svg.querySelector('#'+ id);
+        const country = new Country(id, name, element, visited);
+        region.countries.push(country);
+      }
+    }
+    for (const region of this.regions)
+      region.countries.sort((a, b) => a.name.localeCompare(b.name));
+    this.element = svg;
   }
-});
+}
 
+Promise.all([
+  Map.create(),
+  new Promise(x => window.addEventListener('DOMContentLoaded', x, false))
+]).then(onMapLoaded);
+
+async function onMapLoaded([map]) {
+  const $ = document.querySelector.bind(document);
+  // Append map to DOM.
+  const container = $('.map');
+  container.appendChild(map.element);
+
+  // Build sidebar.
+  const countrylist = $('.countrylist');
+  for (const region of map.regions) {
+    const visitedCountries = region.countries.filter(country => country.visited).length;
+    const regionElement = document.createElement('div');
+    regionElement.classList.add('region');
+    regionElement.innerHTML = `
+      <div class=region-title>
+        <h3>${region.name}</h3><span>${visitedCountries}/${region.countries.length}</span>
+      </div>
+    `;
+
+    countrylist.appendChild(regionElement);
+    for (const country of region.countries)
+      regionElement.appendChild(country.sidebarElement);
+  }
+
+  // Update title
+  const countries = map.regions.reduce((all, region) => [...all, ...region.countries], []);
+  const totalVisited = countries.filter(c => c.visited).length;
+  $('header h3').textContent = `Visited: ${totalVisited}/${countries.length}`;
+
+  // Handle click on map to scroll sidebar.
+  map.element.addEventListener('click', event => {
+    let target = event.target;
+    while (target && !target.classList.contains('land'))
+      target = target.parentElement;
+    const country = target ? target[Country.Symbol] : null;
+    if (!country)
+      return;
+    country.sidebarElement.scrollIntoView({block2: '', inline: 'center', behavior: 'smooth'});
+  }, false);
+
+  // Handle click for sidebar
+  countrylist.addEventListener('mousemove', revealCountry, false);
+  countrylist.addEventListener('mouseleave', revealCountry, false);
+
+  let revealedCountry = null;
+  function revealCountry(mouseEvent) {
+    let target = mouseEvent.target;
+    let country = null;
+    while (target && !(country = target[Country.Symbol]))
+      target = target.parentElement;
+    if (revealedCountry)
+      revealedCountry.mapElement.classList.remove('revealing');
+    revealedCountry = country;
+    if (revealedCountry)
+      revealedCountry.mapElement.classList.add('revealing');
+  }
+};
