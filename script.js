5f2f42324aa799762c33a85a057de5c5c2609cdc
Andrey Lushnikov <lushnikov@chromium.org>
2018-07-03 05:49:42 +0300

    fix: use proven polyfill for scrollIntoViewIfNeeded

--- script.js
+++ script.js
@@ -108,10 +108,8 @@ async function onMapLoaded([map]) {
   // Hover countries when hovering over
   countrylist.addEventListener('mousemove', hoverCountry, false);
   countrylist.addEventListener('mouseleave', hoverCountry, false);
-  countrylist.addEventListener('touchend', hoverCountry, false);
   map.element.addEventListener('mousemove', hoverCountry, false);
   map.element.addEventListener('mouseleave', hoverCountry, false);
-  map.element.addEventListener('touchend', hoverCountry, false);
 
   // Reveal country when clicking
   map.element.addEventListener('click', revealCountry, false);
@@ -167,13 +165,32 @@ async function onMapLoaded([map]) {
   }
 };
 
-function scrollIntoViewIfNeeded(target) {
-  const rect = target.getBoundingClientRect();
-  const element = target.ownerDocument.elementFromPoint(rect.x + rect.width / 2, rect.y + rect.height / 2);
-  while (element && element !== target)
-    element = element.parentElement;
-  if (!element) {
-    target.scrollIntoView({block2: '', inline: 'center', behavior: 'smooth'});
+// Taken from https://gist.github.com/hsablonniere/2581101
+function scrollIntoViewIfNeeded(element, centerIfNeeded = true) {
+  if (element.scrollIntoViewIfNeeded) {
+    element.scrollIntoViewIfNeeded(true);
     return;
+  }
+
+  var parent = element.parentNode,
+      parentComputedStyle = window.getComputedStyle(parent, null),
+      parentBorderTopWidth = parseInt(parentComputedStyle.getPropertyValue('border-top-width')),
+      parentBorderLeftWidth = parseInt(parentComputedStyle.getPropertyValue('border-left-width')),
+      overTop = element.offsetTop - parent.offsetTop < parent.scrollTop,
+      overBottom = (element.offsetTop - parent.offsetTop + element.clientHeight - parentBorderTopWidth) > (parent.scrollTop + parent.clientHeight),
+      overLeft = element.offsetLeft - parent.offsetLeft < parent.scrollLeft,
+      overRight = (element.offsetLeft - parent.offsetLeft + element.clientWidth - parentBorderLeftWidth) > (parent.scrollLeft + parent.clientWidth),
+      alignWithTop = overTop && !overBottom;
+
+  if ((overTop || overBottom) && centerIfNeeded) {
+    parent.scrollTop = element.offsetTop - parent.offsetTop - parent.clientHeight / 2 - parentBorderTopWidth + element.clientHeight / 2;
+  }
+
+  if ((overLeft || overRight) && centerIfNeeded) {
+    parent.scrollLeft = element.offsetLeft - parent.offsetLeft - parent.clientWidth / 2 - parentBorderLeftWidth + element.clientWidth / 2;
+  }
+
+  if ((overTop || overBottom || overLeft || overRight) && !centerIfNeeded) {
+    element.scrollIntoView(alignWithTop);
   }
 }
